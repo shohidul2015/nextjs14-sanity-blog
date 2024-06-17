f408352b5a4c6619a0dbe71630c786ea8f0c3d02
shohidul2015 <shd_islam@hotmail.com>
2024-06-17 18:48:55 +0200

    Disabling the ESLint Rule

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { singleBlog } from '@/components/lib/interface';
 import Image from 'next/image';
 import { PortableText } from 'next-sanity';
 
+export const revalidate = 30; // refresh sanity data in very 30 sec
+
 async function getData(slug: string) {
     const query = `
         *[_type=='blog' && slug.current=='${slug}'] {
